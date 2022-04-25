a37a8d82c1d45e8975cfaa0f8f43f0be9bc2403e
kzaknsok <89708310+kzaknsok@users.noreply.github.com>
2022-04-25 22:07:20 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -64,12 +64,54 @@ import "./styles.css";
  * .lastElementChild
  * .children(n番目)
  * ~.innerTextとすれば取得出来る
+ * エリアをまたぐ時にliの中身を一旦初期化するため
+ * addTargetにnullを代入
+ * (代入するとボタン情報も含めて消してくれる)
+ * li配下に子要素のdivを生成
+ * 更に取得したinnerTextをpタグに代入
+ * この時点でクラスを付与したそれぞれのタグが生成される
+ * (クリックイベントの始めにそれぞれクラス付きのタグを生成
+ * できるようにcreateElementを指示しているからか？)
+ * 完了エリアでは戻すボタンのみ必要、生成する
+ * addTarget(li)配下にdiv
+ * div配下にp,buttonをそれぞれappendChild
+ * console.log(addTarget)でliの中身確認
+ * document.getElementById("complete-list").appendChild(addTarget);
+ * でid付きのul配下に要素を生成
+ *
+ * 戻すボタン
+ * クリックと同時にulからliごと要素を削除
+ * pタグのテキストを変数textに格納
+ * const text = backButton.parentNode.firstElementChild.innerText;
+ *
+ *
+ * 関数化について。。。
+ * 共通の処理は関数化する、異なる部分は引数でまかなう
+ * li生成からテキストの登録までを関数化
+ *   createIncompleteList
+ * 関数を定義した時点での引数はtextとしておく
+ * 最初のtodo登録時の値がinputTextであるため、
+ * 追加ボタンのクリックイベント時のみ
+ *   createIncompleteList(inputText);
+ * とする事で、各セクションでの共通の動きを関数化しつつ
+ * それぞれに対応した引数を渡す事で正しくテキストのやり取りが
+ * 実現できる
  *
  */
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+};
+
+// 未完了エリアからテキストとボタンの削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了エリアに追加する関数
+const createIncompleteList = (text) => {
   // 外枠のli生成
   const li = document.createElement("li");
 
@@ -80,21 +122,49 @@ const onClickAdd = () => {
   // p生成
   const p = document.createElement("p");
   p.className = "list-index";
-  p.innerText = inputText;
+  p.innerText = text;
 
   // 完了buttonの生成
   const complateButton = document.createElement("button");
   complateButton.innerText = "完了";
-  // 完了ボタンイベントの確認
+  // 完了ボタンイベント
   complateButton.addEventListener("click", () => {
-    deleteFromInconpleteList(complateButton.closest("li"));
+    // 押したらボタンを削除
+    deleteFromIncompleteList(complateButton.closest("li"));
 
     // 完了した項目を完了エリアに表示させる
     const addTarget = complateButton.closest("li");
 
     // 追加したpタグのテキストを取得、textに格納
     const text = addTarget.querySelector("p").innerText;
-    console.log(text);
+
+    // li以下の初期化、完了動作と同時にdiv,p,情報を削除
+    addTarget.textContent = null;
+
+    //取得したtextを含んだpを生成
+    const div = document.createElement("div");
+    div.className = "list-row";
+    p.innerText = text;
+
+    //buttonの生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 戻すボタンを押したらliを削除
+      const deleteTarget = backButton.closest("li");
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      //戻すボタン押したらtext情報を取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
+
+    //liタグの配下に各要素設定
+    addTarget.appendChild(div);
+    div.appendChild(p);
+    div.appendChild(backButton);
+
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // 削除buttonの生成
@@ -103,7 +173,7 @@ const onClickAdd = () => {
   // 削除ボタンイベントの確認(alert)
   // 削除の動作に連動して親要素のliタグごと削除する
   deleteButton.addEventListener("click", () => {
-    deleteFromInconpleteList(deleteButton.closest("li"));
+    deleteFromIncompleteList(deleteButton.closest("li"));
   });
 
   // liタグの子要素としてdivタグ
@@ -114,11 +184,6 @@ const onClickAdd = () => {
   div.appendChild(deleteButton);
 
   document.getElementById("incomplete-list").appendChild(li);
-};
-
-// 未完了エリアからテキストとボタンの削除
-const deleteFromInconpleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
